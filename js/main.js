8b43250622de4bb30ffee2d98465550edcc720e1
Viraj Jain <harshal.jain.csai.20@ggits.net>
2024-02-29 16:56:58 +0530

    light

--- js/main.js
+++ js/main.js
@@ -41,14 +41,14 @@ function setSkyBox() {
   scene.add(skybox);
 }
 
-function loadPlanetTexture(texture, radius, widthSegments, heightSegments) {
+function loadPlanetTexture(texture, radius, widthSegments, heightSegments, meshType) {
   const geometry = new THREE.SphereGeometry(radius, widthSegments, heightSegments);
   const loader = new THREE.TextureLoader();
   const planetTexture = loader.load(texture);
-  const material = new THREE.MeshBasicMaterial({ map: planetTexture });
+  const material = meshType == 'standard' ? new THREE.MeshStandardMaterial({ map: planetTexture }) : new THREE.MeshBasicMaterial({ map: planetTexture });
 
   const planet = new THREE.Mesh(geometry, material);
-  
+
   return planet
 }
 
@@ -57,10 +57,10 @@ function loadPlanetTexture(texture, radius, widthSegments, heightSegments) {
 
 
 function createRing(innerRadius) {
-  let outerRadius = innerRadius - 0.2
+  let outerRadius = innerRadius - 0.01
   let thetaSegments = 100
   const geometry = new THREE.RingGeometry(innerRadius, outerRadius, thetaSegments);
-  const material = new THREE.MeshBasicMaterial({ color: '#0f0f0f', side: THREE.DoubleSide });
+  const material = new THREE.MeshBasicMaterial({ color: '#ffffff', side: THREE.DoubleSide });
   const mesh = new THREE.Mesh(geometry, material);
   scene.add(mesh)
   mesh.rotation.x = Math.PI / 2
@@ -79,15 +79,15 @@ function init() {
   );
 
   setSkyBox();
-  planet_earth = loadPlanetTexture("../img/earth_hd.jpg", 4, 100, 100);
-  planet_sun = loadPlanetTexture("../img/sun_hd.jpg", 20, 100, 100);
-  planet_mercury = loadPlanetTexture("../img/mercury_hd.jpg", 2, 100, 100);
-  planet_venus = loadPlanetTexture("../img/venus_hd.jpg", 3, 100, 100);
-  planet_mars = loadPlanetTexture("../img/mars_hd.jpg", 3.5, 100, 100);
-  planet_jupiter = loadPlanetTexture("../img/jupiter_hd.jpg", 10, 100, 100);
-  planet_saturn = loadPlanetTexture("../img/saturn_hd.jpg", 8, 100, 100);
-  planet_uranus = loadPlanetTexture("../img/uranus_hd.jpg", 6, 100, 100);
-  planet_neptune = loadPlanetTexture("../img/neptune_hd.jpg", 5, 100, 100);
+  planet_earth = loadPlanetTexture("../img/earth_hd.jpg", 4, 100, 100, 'standard');
+  planet_sun = loadPlanetTexture("../img/sun_hd.jpg", 20, 100, 100, 'basic');
+  planet_mercury = loadPlanetTexture("../img/mercury_hd.jpg", 2, 100, 100, 'standard');
+  planet_venus = loadPlanetTexture("../img/venus_hd.jpg", 3, 100, 100, 'standard');
+  planet_mars = loadPlanetTexture("../img/mars_hd.jpg", 3.5, 100, 100, 'standard');
+  planet_jupiter = loadPlanetTexture("../img/jupiter_hd.jpg", 10, 100, 100, 'standard');
+  planet_saturn = loadPlanetTexture("../img/saturn_hd.jpg", 8, 100, 100, 'standard');
+  planet_uranus = loadPlanetTexture("../img/uranus_hd.jpg", 6, 100, 100, 'standard');
+  planet_neptune = loadPlanetTexture("../img/neptune_hd.jpg", 5, 100, 100, 'standard');
 
   // planet_earth_label = new THREE.TextGeometry( text, parameters );
   // planet_mercury_label = loadPlanetTexture("../img/mercury_hd.jpg", 2, 100, 100);
@@ -108,6 +108,10 @@ function init() {
   scene.add(planet_saturn);
   scene.add(planet_uranus);
   scene.add(planet_neptune);
+
+  const sunLight = new THREE.PointLight(0xffffff, 1, 0); // White light, intensity 1, no distance attenuation
+  sunLight.position.copy(planet_sun.position); // Position the light at the Sun's position
+  scene.add(sunLight);
 
   // Rotation orbit
   createRing(mercury_orbit_radius)
@@ -134,9 +138,9 @@ function init() {
 }
 
 
-function planetRevolver(time,speed, planet, orbitRadius, planetName) {
+function planetRevolver(time, speed, planet, orbitRadius, planetName) {
 
-  let orbitSpeedMultiplier =  0.001;
+  let orbitSpeedMultiplier = 0.001;
   const planetAngle = time * orbitSpeedMultiplier * speed;
   planet.position.x = planet_sun.position.x + orbitRadius * Math.cos(planetAngle);
   planet.position.z = planet_sun.position.z + orbitRadius * Math.sin(planetAngle);
@@ -165,14 +169,14 @@ function animate(time) {
   // const earthOrbitAngle = time * orbitSpeedMultiplier;
   // planet_earth.position.x = planet_sun.position.x + earth_orbit_radius * Math.cos(earthOrbitAngle);
   // planet_earth.position.z = planet_sun.position.z + earth_orbit_radius * Math.sin(earthOrbitAngle);
-  planetRevolver(time, mercury_revolution_speed, planet_mercury, mercury_orbit_radius,'mercury')
-  planetRevolver(time, venus_revolution_speed, planet_venus, venus_orbit_radius,'venus')
-  planetRevolver(time, earth_revolution_speed, planet_earth, earth_orbit_radius,'earth')
-  planetRevolver(time, mars_revolution_speed, planet_mars, mars_orbit_radius,'mars')
-  planetRevolver(time, jupiter_revolution_speed, planet_jupiter, jupiter_orbit_radius,'jupiter')
-  planetRevolver(time, saturn_revolution_speed, planet_saturn, saturn_orbit_radius,'saturn')
-  planetRevolver(time, uranus_revolution_speed, planet_uranus, uranus_orbit_radius,'uranus')
-  planetRevolver(time, neptune_revolution_speed, planet_neptune, neptune_orbit_radius,'neptune')
+  planetRevolver(time, mercury_revolution_speed, planet_mercury, mercury_orbit_radius, 'mercury')
+  planetRevolver(time, venus_revolution_speed, planet_venus, venus_orbit_radius, 'venus')
+  planetRevolver(time, earth_revolution_speed, planet_earth, earth_orbit_radius, 'earth')
+  planetRevolver(time, mars_revolution_speed, planet_mars, mars_orbit_radius, 'mars')
+  planetRevolver(time, jupiter_revolution_speed, planet_jupiter, jupiter_orbit_radius, 'jupiter')
+  planetRevolver(time, saturn_revolution_speed, planet_saturn, saturn_orbit_radius, 'saturn')
+  planetRevolver(time, uranus_revolution_speed, planet_uranus, uranus_orbit_radius, 'uranus')
+  planetRevolver(time, neptune_revolution_speed, planet_neptune, neptune_orbit_radius, 'neptune')
 
 
 
